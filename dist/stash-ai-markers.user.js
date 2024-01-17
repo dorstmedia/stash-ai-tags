d51887877aee5f28ce529507eab5b02847ff2895
dorstmedia <86899017+dorstmedia@users.noreply.github.com>
2024-01-17 08:00:54 +0100

    Update stash-ai-markers.user.js

--- dist/stash-ai-markers.user.js
+++ dist/stash-ai-markers.user.js
@@ -3,7 +3,7 @@
 // @description Generate markers for a scene
 // @icon        https://raw.githubusercontent.com/dorstmedia/stash-ai-tags/main/_media/stashapp-favicon.ico
 // @namespace   https://github.com/dorstmedia/stash-ai-tags
-// @version     0.2.1.3
+// @version     0.2.1.4
 // @homepage    https://github.com/dorstmedia/stash-ai-tags/blob/main/dist/stash-ai-markers.user.js
 // @author      dorstmedia (forked from cc12344567)
 // @resource    css https://raw.githubusercontent.com/dorstmedia/stash-ai-tags/main/dist/bundle.markers.css
