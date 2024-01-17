19859398ac12e58c0c4e21d61eb8e417b28e1088
dorstmedia <86899017+dorstmedia@users.noreply.github.com>
2024-01-17 07:48:20 +0100

    Update stash-ai-runner.user.js

--- dist/stash-ai-runner.user.js
+++ dist/stash-ai-runner.user.js
@@ -3,8 +3,8 @@
 // @description Find tags for a scene
 // @icon        https://docs.stashapp.cc/favicon.ico
 // @namespace   https://github.com/dorstmedia
-// @version     0.1.1
-// @homepage    https://github.com/dorstmedia/stash-ai-tags
+// @version     0.1.1.1
+// @homepage    https://github.com/dorstmedia/stash-ai-tags/blob/main/dist/stash-ai-runner.user.js
 // @author      dorstmedia (forked from cc12344567)
 // @match       http://localhost:9999/*
 // @exclude     http://localhost:9999/settings?tab=logs
